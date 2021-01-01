9cf4a625a06a722a6a233127116dc00c309e845e
JuanjoCarre <juanjopalacios1@gmail.com>
2021-01-01 20:26:14 +0100

    Cambiando el branch cambio de variable

--- hello.f90
+++ hello.f90
@@ -1,5 +1,5 @@
 program hello
-    character(20):myname
-    read *, ,myname
-        write *,"Hello word aqui programando git ",myname
+    character(20):myname11
+    read *, ,myname11
+        write *,"Hello word aqui programando git ",myname11
 end program hello
